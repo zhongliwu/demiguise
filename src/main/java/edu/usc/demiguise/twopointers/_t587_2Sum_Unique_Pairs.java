6cb519b38e39e76bfc3090882b37902a204de90a
zhongliwu <zhongliwu2015@hotmail.com>
2023-12-11 20:59:58 +0800

    lintcode - 587 commit

---
+++ src/main/java/edu/usc/demiguise/twopointers/_t587_2Sum_Unique_Pairs.java
@@ -0,0 +1,39 @@
+package edu.usc.demiguise.twopointers;
+
+import java.util.Arrays;
+
+public class _t587_2Sum_Unique_Pairs {
+    public int twoSum6(int[] nums, int target) {
+        // write your code here
+        if (nums == null && nums.length < 2) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int numOfTuples = 0;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int curSum = nums[left] + nums[right];
+            if (curSum == target) {
+                numOfTuples += 1;
+                left += 1;
+                right -= 1;
+
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left += 1;
+                }
+
+                while (left < right && nums[right] == nums[right + 1]) {
+                    right -= 1;
+                }
+
+            } else if (curSum > target) {
+                right -= 1;
+            } else {
+                left += 1;
+            }
+        }
+
+        return numOfTuples;
+    }
+}
